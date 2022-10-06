714fecf5b2900fea64a16543e4de3470d5322241
Edson Lucas <80084878+Edson-html-css-js@users.noreply.github.com>
2022-10-06 19:36:44 -0300

    Raciocinio logico, imc,par e impar, tratamento logico em login

---
+++ exer_lista2/par_e_impar.py
@@ -0,0 +1,9 @@
+
+num=int(input("digite um numero inteiro: "))
+
+soma = num%2
+
+if soma == 0:
+    print("esse numero é par")
+elif soma == 1:
+        print("esse numero é impar")
---
+++ exer_lista2/tratamento_logico_com_login.py
@@ -0,0 +1,9 @@
+user = input("user: ")
+senha= input("password: ")
+
+if (user=='procopio' and senha=='12345') or (user=='paiva' and senha=='54321'):
+        print("você logou | seja bem vindo")
+
+else:
+    print("acesso negado, usuario ou senha está errado")        
+
